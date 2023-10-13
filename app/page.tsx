f5ea38ee0a81e1d5e7515d53193660cccda212fc
thatcatfromspace <encyclopedia748@gmail.com>
2023-10-14 02:25:48 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import React from 'react'
+import Bolt from '../public/bolt.svg'
+import Vector from '../public/Vector.svg'
+import Folder from '../public/folder.svg'
+import Graph from '../public/graph.svg'
+import Brain from '../public/neural-brain-icon.svg'
+import People from '../public/people.svg'
+import User from '../public/user.svg'
+import Search from '../public/search.svg'
+
+import { useState } from 'react';
+
+const page = () => {
+
+  const [stateOne, setStateOne] = useState(true);
+  const [stateTwo, setStateTwo] = useState(true);
+  const [stateThree, setStateThree] = useState(true);
+  const [stateFour, setStateFour] = useState(true);
+
+  const [makeTagOne, setTagOne] = useState(true);
+  const [makeTagTwo, setTagTwo] = useState(true);
+  const [makeTagThree, setTagThree] = useState(true);
+  const [makeTagFour, setTagFour] = useState(true);
+
+  const setStateOneFunc = () => {
+    setStateOne(!stateOne)
+  }
+
+  const setStateTwoFunc = () => {
+    setStateOne(!stateOne)
+  }
+
+  const setStateThreeFunc = () => {
+    setStateOne(!stateOne)
+  }
+
+  const setStateFourFunc = () => {
+    setStateOne(!stateOne)
+  }
+
+  const setMakeTagOne = () => {
+    setTagOne(!makeTagOne);
+  }
+
+  const setMakeTagTwo = () => {
+    setTagTwo(!makeTagTwo);
+  }
+
+  const setMakeTagThree = () => {
+    setTagThree(!makeTagThree);
+  }
+
+  const setMakeTagFour = () => {
+    setTagFour(!makeTagFour);
+  }
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex'>
+      <nav className='navbar flex flex-col items-center justify-start w-20 min-h-[100vh] bg-[#4C1364]'>
+        <div className='py-4'>
+          <Brain /> 
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className='h-[1px] bg-black px-7'></div>
+        <div className='py-4'>
+          <Vector />
+        </div>
+        <div className='py-4'>
+          <Search />
+        </div>
+        <div className='py-4'>
+          <Bolt />
+        </div>
+        <div className='py-4'>
+          <Graph />
+        </div>
+        <div className='py-4'>
+          <People />
+        </div>
+        <div className='py-4'>
+          <Folder />
+        </div>
+        <div className='py-4'>
+          <User />
+        </div>
+      </nav>
+      <main className='flex-col w-full px-10 py-10'>
+        <div className='text-blue-500 py-10 text-xl'>Add a Filament</div>
+        <form className='flex flex-col justify-center h-32 rounded-2xl w-[75vw] max-w-72 bg-[#4C1364]'>
+          <input type='text' placeholder='Drop into your Second Brain' className='bg-[#5B1478] w-[40%] ms-10 h-12 my-auto ps-4 rounded-2xl'></input>
+        </form>
+        <div className='flex w-[50vw] justify-evenly py-12'>
+          <div className='flex items-center'>
+            <div className='flex items-center justify-center w-[16px] h-[16px] cursor-pointer bg-purple-500 rounded-full' onClick={setStateOneFunc}>
+              <div className={`w-[10px] h-[10px] rounded-full ${stateOne? 'bg-yellow-400': 'bg-transparent'} `}></div> 
+            </div>
+            <label htmlFor='spark' className='text-slate-200 ms-3'> Spark </label>
+          </div>
+          <div className='flex items-center'>
+            <div className='flex items-center justify-center w-[16px] h-[16px] cursor-pointer bg-purple-500 rounded-full' onClick={setStateTwoFunc}>
+              <div className={`w-[10px] h-[10px] rounded-full ${stateOne? 'bg-green-400': 'bg-transparent'} `}></div> 
+            </div>
+            <label htmlFor='people' className='text-slate-200 ms-3'> People </label>
+          </div>
+          <div className='flex items-center'>
+            <div className='flex items-center justify-center w-[16px] h-[16px] cursor-pointer bg-purple-500 rounded-full' onClick={setStateThreeFunc}>
+              <div className={`w-[10px] h-[10px] rounded-full ${stateOne? 'bg-blue-400': 'bg-transparent'} `}></div> 
+            </div>
+            <label htmlFor='space' className='text-slate-200 ms-3'> Space </label>
+          </div>
+          <div className='flex items-center'>
+            <div className='flex items-center justify-center w-[16px] h-[16px] cursor-pointer bg-purple-500 rounded-full' onClick={setStateFourFunc}>
+              <div className={`w-[10px] h-[10px] rounded-full ${stateOne? 'bg-red-400': 'bg-transparent'} `}></div> 
+            </div>
+            <label htmlFor='resource' className='text-slate-200 ms-3'> Resource </label>
+          </div>
+        </div>
+        <div className='flex justify-center w-[50vw] max-vw-[72] pt-16 px-36'>
+          <select name='chamber' className='bg-[#4C1364] w-48 text-slate-200 rounded-2xl py-4 ps-4 pe-10 me-10'>
+            <option value="op1">Chamber</option>
+            <option value="op2">Option 2</option>
+            <option value="op3">Option 3</option>
+            <option value="op4">Option 4</option>
+          </select>
+          <select name='vault' className='bg-[#4C1364] w-48 text-slate-200 rounded-2xl py-4 ps-4 pe-10'>
+            <option value="op1">Vault</option>
+            <option value="op2">Option 2</option>
+            <option value="op3">Option 3</option>
+            <option value="op4">Option 4</option>
+          </select>
+        </div>
+        <div className='flex items-center bg-[#4C1364] w-[45vw] rounded-full h-12 mt-16'>
+          <div className='px-7 flex text-slate-200 '>Tags</div>
+          <div className={`tag ${makeTagOne? 'flex': 'hidden'}  text-xs bg-yellow-300`}> Spark 
+            <div className={`flex justify-center items-center ms-1 w-[20px] h-[20px]  text-gray-500 rounded-full cursor-pointer hover:bg-slate-300`} onClick={setMakeTagOne}>x</div>
+          </div>
+          <div className={`tag ${makeTagTwo? 'flex': 'hidden'} text-xs bg-green-500`}> People
+            <div className='flex justify-center items-center ms-1 w-[20px] h-[20px]  text-gray-500 rounded-full cursor-pointer hover:bg-slate-300' onClick={setMakeTagTwo}>x</div>
+          </div>
+          <div className={`tag ${makeTagThree? 'flex': 'hidden'} text-xs bg-blue-500`}> Space
+            <div className='flex justify-center items-center ms-1 w-[20px] h-[20px]  text-gray-500 rounded-full cursor-pointer hover:bg-slate-300' onClick={setMakeTagThree}>x</div>
+          </div>
+          <div className={`tag ${makeTagFour? 'flex': 'hidden'} text-xs bg-red-500`}> Resource 
+            <div className='flex justify-center items-center ms-1 w-[20px] h-[20px]  text-gray-500 rounded-full cursor-pointer hover:bg-slate-300' onClick={setMakeTagFour}>x</div>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
+
+export default page
